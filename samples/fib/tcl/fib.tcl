e9eb348d1523eafd836c399f52f1f3eacd1309c4
Detlef Groth (D7480) <dgroth@mura.de>
2026-02-01 09:35:34 +0000

    adding Fibonacci number examples

--- samples/fib/tcl/fib.tcl
+++ samples/fib/tcl/fib.tcl
@@ -1,15 +1,4 @@
 #!/usr/bin/env tclsh
-##############################################################################
-#
-# Copyright (C) 2026 MicroEmacs User.
-#
-# All rights reserved.
-#
-# Synopsis:    
-# Authors:     MicroEmacs User
-#
-##############################################################################
-
 load ./fib.so Fib; 
 foreach x [list 1 2 3 4 5] { 
     puts "fin of $x is: [Fib_fib $x]"
--- samples/hello/go/run-hello.go
+++ samples/hello/go/run-hello.go
@@ -8,15 +8,15 @@ import (
 )
 
 func main() {
-    argv := os.Args
-    cGreeting := C.Hello_GetMessage()
-    if cGreeting == nil {
-	fmt.Println("Error: C-function gave back null!")
-	return
-    }
-    // C-String back to Go-String 
-    goGreeting := C.GoString(cGreeting)
-    fmt.Printf("Go gets: %s\n", goGreeting)    
-    fmt.Println("Hello World from Go!")
-    fmt.Printf("Usage: %s\n",argv[0])
-}   
+	argv := os.Args
+	cGreeting := C.Hello_GetMessage()
+	if cGreeting == nil {
+		fmt.Println("Error: C-function gave back null!")
+		return
+	}
+	// C-String back to Go-String 
+	goGreeting := C.GoString(cGreeting)
+	fmt.Printf("Go gets: %s\n", goGreeting)	
+	fmt.Println("Hello World from Go!")
+	fmt.Printf("Usage: %s\n",argv[0])
+}
